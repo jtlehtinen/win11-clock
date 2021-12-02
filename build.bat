a8ea9a61cd5136d905c0390345cd24930d32a747
Juha Lehtinen <jtlehtinen@fastmail.com>
2021-12-02 12:41:40 +0200

    icon and a pixel shader that was used to create the icon

--- build.bat
+++ build.bat
@@ -7,16 +7,21 @@ if not exist build\release ( mkdir build\release )
 set cflags=/nologo /Zi /std:c++20 /EHsc /Wall /WX /wd5039
 set lflags=/INCREMENTAL:NO /subsystem:windows
 
+rc /nologo /fo resources.res src\resources.rc
+copy /Y resources.res build\debug\resources.res 1>nul
+copy /Y resources.res build\release\resources.res 1>nul
+del resources.res
+
 pushd build\debug
 echo ----------------
 echo building debug:
-cl %cflags% /Feclock_debug.exe /Od /DCLOCK_DEBUG ..\..\src\main.cpp /link %lflags%
+cl %cflags% /Feclock_debug.exe /Od /DCLOCK_DEBUG ..\..\src\main.cpp resources.res /link %lflags%
 del *.obj
 popd
 
 pushd build\release
 echo ----------------
 echo building release:
-cl %cflags% /Feclock.exe /Oi /O2 ..\..\src\main.cpp /link %lflags%
+cl %cflags% /Feclock.exe /Oi /O2 ..\..\src\main.cpp resources.res /link %lflags%
 del *.obj
 popd
